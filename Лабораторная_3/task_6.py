ecfa80cc0948be31f7a5f053a9168f67c990e932
Bekishev-Sergey <115820346+Bekishev-Sergey@users.noreply.github.com>
2022-10-16 10:26:28 +0300

    Update task_6.py

--- Лабораторная_3/task_6.py
+++ Лабораторная_3/task_6.py
@@ -1,10 +1,14 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO Оформить решение
-num = max(list_numbers)
-index = list_numbers.index(num)
-o_o = list_numbers[-1]
-list_numbers[-1] = num
-list_numbers[index] = o_o
+last_num = list_numbers[-1]
+max_value, max_index = 0, 0
+
+for i, value in enumerate(list_numbers):
+    if value > max_value:
+        max_value = value
+        max_index = i
+list_numbers[max_index] = last_num
+list_numbers[-1] = max_value
 
 print(list_numbers)
