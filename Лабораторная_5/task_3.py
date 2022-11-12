fbdde00e54c2226197f1631569278c351dcadffc
Bekishev-Sergey <115820346+Bekishev-Sergey@users.noreply.github.com>
2022-11-12 09:53:01 +0300

    Лабораторная работа 5, попытка 3

--- Лабораторная_5/task_3.py
+++ Лабораторная_5/task_3.py
@@ -1,9 +1,13 @@
 import random
 def get_unique_list_numbers(start=-10, finish=10, size=15) -> list[int]:
     password = []
+    number_of_allowed_characters = range(start, finish + 1)
     while len(password) != size:# TODO написать функцию для получения списка уникальных целых чисел
         symbol = random.randint(start, finish)
-        if symbol not in password:
+        if size > len(number_of_allowed_characters):
+            print("size of password is too long")
+            break
+        elif symbol not in password:
             password.append(symbol)
     return password
 
